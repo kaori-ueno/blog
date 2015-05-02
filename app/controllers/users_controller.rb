9e0cd42c77bebe938c5e50f8860f110b934a36f0
upscent <upscent@gmail.com>
2015-05-02 21:41:48 +0900

    キーワード配列の書き方修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authorize
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: %i(show edit update destroy)
 
   # GET /users
   # GET /users.json
