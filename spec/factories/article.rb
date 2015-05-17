64a97ba95ddccb297e4b704857ed4ea425f1f0a8
upscent <upscent@gmail.com>
2015-05-17 23:36:30 +0900

    factory 修正

--- spec/factories/article.rb
+++ spec/factories/article.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :article do
     title "Title"
     body "Body"
+    association :blog
   end
 end
--- spec/factories/blog.rb
+++ spec/factories/blog.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :blog do
     name "Blog"
+    association :user
   end
 end
