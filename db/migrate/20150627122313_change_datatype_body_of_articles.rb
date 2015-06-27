e5fbde9213c44dfc93db3261381cd5ea245ed04c
upscent <upscent@gmail.com>
2015-06-27 21:27:10 +0900

    articlesのbodyをtextに変更

---
+++ db/migrate/20150627122313_change_datatype_body_of_articles.rb
@@ -0,0 +1,5 @@
+class ChangeDatatypeBodyOfArticles < ActiveRecord::Migration
+  def change
+    change_column :articles, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150508125944) do
+ActiveRecord::Schema.define(version: 20150627122313) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.integer  "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
