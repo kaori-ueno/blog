07cf8f825d87da6f278d964ddfd7feeb0cf1fc81
upscent <upscent@gmail.com>
2015-06-27 12:32:33 +0900

    時間表示関数をhelperに追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_ja_datetime(datetime)
+    datetime.strftime("%Y年%m月%d日 %H:%M:%S")
+  end
+
+  def format_ja_date(datetime)
+    datetime.strftime("%Y年%m月%d日")
+  end
 end
