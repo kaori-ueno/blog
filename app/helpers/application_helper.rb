7fd84714eb588ff1332dc16e7eaffed487cfd6c2
upscent <upscent@gmail.com>
2015-06-29 10:34:13 +0900

    テキスト整形メソッド追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def format_ja_date(datetime)
     datetime.strftime("%Y年%m月%d日")
   end
+
+  def format_text(text)
+    text = h text
+    text = text.gsub /\r\n|\r|\n/, "<br />"
+    text.html_safe
+  end
 end
