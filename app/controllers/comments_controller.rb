9399a310925962be251107d9c9e1281f2693117f
upscent <upscent@gmail.com>
2015-06-29 20:21:04 +0900

    コメント作成後のリダイレクト先修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,8 +16,8 @@ class CommentsController < ApplicationController
   def create
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to @comment.article, notice: "Comment was successfully created." }
+        format.json { render :show, status: :created, article_id: @comment.article }
       else
         format.html { render :new, article_id: @comment.article }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
